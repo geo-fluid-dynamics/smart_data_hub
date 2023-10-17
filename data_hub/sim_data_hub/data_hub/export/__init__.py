b4b43b87e92a3545dc8a21bbd67b2d9eca15aa09
CQVera <qian.chen1@rwth-aachen.de>
2023-10-17 11:59:56 +0200

    added the submodule

--- data_hub/sim_data_hub/data_hub/export/__init__.py
+++
@@ -1,9 +0,0 @@
-###############################################################
-# 
-# bundle the ice data hub Regime modules
-# Marc @ RWTH, March 2020
-#
-################################################################
-
-from .nexd import *
-from .trajectory import *
