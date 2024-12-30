0a7444c1ab3728a2c3d0a188664723c2b8b3916d
CQVera <qian.chen1@rwth-aachen.de>
2024-12-30 13:02:01 +0100

    Added structural group

--- gempy_to_plydata.py
+++ gempy_to_plydata.py
@@ -5,20 +5,25 @@ import pyvista as pv
 import numpy as np
 
 
-def create_geomodel_for_site(site_orientation_path: str, site_points_path: str, extent_geometry: list = None,
-                             save_ply_folder_path: str = ''):
+def create_geomodel_for_site(
+        site_orientation_path: str,
+        site_points_path: str,
+        extent_geometry: list = None,
+        structural_group: dict = None,
+        save_ply_folder_path: str = "",
+):
     """
     Create GemPy 3D structural model and save each surface mesh to PolyData
     @param site_orientation_path: path to CSV file that stores orientation data for each surface.
     @param site_points_path: path to CSV file that stores point data for each surface.
     @param extent_geometry: model extend in a list [x_min, x_max, y_min, y_max, z_min, z_max].
+    @param structural_group: series or stacks in the model.
     @param save_ply_folder_path: path to a folder that saves the PolyData.
     @return: PolyData for each surface.
     """
 
     points_csv = pd.read_csv(site_points_path)
-    # get strata name list
-    formation_list = list(dict.fromkeys(list(points_csv["formation"])))
+
     if extent_geometry is None:
         extent_geometry = [
             min(0, min(list(points_csv["X"]))),
@@ -28,7 +33,7 @@ def create_geomodel_for_site(site_orientation_path: str, site_points_path: str, 
             min(0, min(list(points_csv["Z"]))),
             max(0, max(list(points_csv["Z"]))),
         ]
-    formation_dict = dict(zip(formation_list, formation_list))
+
     geo_data = gp.create_geomodel(
         project_name="site",
         extent=extent_geometry,
@@ -38,7 +43,14 @@ def create_geomodel_for_site(site_orientation_path: str, site_points_path: str, 
             path_to_surface_points=site_points_path,
         ),
     )
-    surface_data = gp.map_stack_to_surfaces(geo_data, formation_dict)
+
+    # get strata name list
+    formation_list = list(dict.fromkeys(list(points_csv["formation"])))
+
+    if structural_group is None:
+        structural_group = {"Strat_series": formation_list}
+
+    surface_data = gp.map_stack_to_surfaces(geo_data, mapping_object=structural_group)
 
     # Compute the geological model
     data_model = gp.compute_model(geo_data)
@@ -52,11 +64,12 @@ def create_geomodel_for_site(site_orientation_path: str, site_points_path: str, 
     ]
     surface_meshes_edges = [mesh.edges for mesh in geo_model.solutions.dc_meshes]
 
-    #
     for i in range(len(surface_meshes_vertices)):
         surface_mesh = pv.PolyData(
             surface_meshes_vertices[i],
             np.insert(surface_meshes_edges[i], 0, 3, axis=1).ravel(),
         )
-        surface_mesh.save(os.path.join(save_ply_folder_path, f"{formation_list[i]}.ply"))
+        surface_mesh.save(
+            os.path.join(save_ply_folder_path, f"{formation_list[i]}.ply")
+        )
         print(f"Saved {formation_list[i]}.ply !")
